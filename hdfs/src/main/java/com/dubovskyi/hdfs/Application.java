2040f7c8c9dcf54986421b0c1d7b3aaed88466bf
Mykhailo <m@m.ua>
2018-11-08 21:44:33 +0200

    add correct connection to hdfs

--- hdfs/src/main/java/com/dubovskyi/hdfs/Application.java
+++ hdfs/src/main/java/com/dubovskyi/hdfs/Application.java
@@ -2,23 +2,41 @@ package com.dubovskyi.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Application {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         Configuration conf = new Configuration ();
-        conf.set("fs.default.name", "hdfs://localhost:9000");
+
+        String path1 = Application.class.getResource("/core-site.xml").getFile();
+        System.out.println(path1);
+        conf.addResource(new Path(path1));
+        String path2 = Application.class.getResource("/hdfs-site.xml").getFile();
+
+        System.out.println(path2);
+        conf.addResource(new Path(path1));
+        conf.addResource(new Path(path2));
+     //  conf.set("fs.default.name", "hdfs://localhost:9000");
 
         FileSystem hdfs = FileSystem.get(conf);
-        Path path = new Path("hdfs://localhost:9000/destination");
+        Path path = new Path("hdfs://sandbox-hdp.hortonworks.com:8020/");
 
-        FSDataInputStream in = hdfs.open(path);
+        FileStatus[] a= hdfs.listStatus(path);
+
+        for(FileStatus status : a){
+            System.out.println(status);
+        }
+
+     /*   FSDataInputStream in = hdfs.open(path);
         BufferedReader br=new BufferedReader(new InputStreamReader(in));
         try {
             String line;
@@ -32,7 +50,7 @@ public class Application {
         } finally {
             // you should close out the BufferedReader
             br.close();
-        }
+        }*/
 
 
 
