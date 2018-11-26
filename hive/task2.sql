d66032df391f1d6c418a9aff23b230a782fad064
Mykhailo <m@m.ua>
2018-11-26 22:03:52 +0200

    - add hive tasks

--- hive/task2.sql
+++ hive/task2.sql
@@ -1,1 +1,1 @@
-select max(datediff(to_date(train.srch_co),to_date(train.srch_ci))) as dur  from train limit 3;
+select max(datediff(to_date(train.srch_co),to_date(train.srch_ci))) as dur  from train where train.srch_adults_cnt =2 and train.srch_children_cnt>0 ;
---
+++ hive/task3.sql
@@ -0,0 +1,4 @@
+select train.hotel_continent,train.hotel_country,train.hotel_market,count(train.user_id) as num
+from train
+where train.is_booking	= 0
+group by train.hotel_continent,train.hotel_country,train.hotel_market;
