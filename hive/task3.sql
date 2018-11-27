8dc3068217354caad3f1e1a2dd5b425e694c5a50
Mykhailo <m@m.ua>
2018-11-27 18:31:58 +0200

    edit task3

--- hive/task3.sql
+++ hive/task3.sql
@@ -1,4 +1,5 @@
 select train.hotel_continent,train.hotel_country,train.hotel_market,count(train.user_id) as num
 from train
 where train.is_booking	= 0
-group by train.hotel_continent,train.hotel_country,train.hotel_market;
+group by train.hotel_continent,train.hotel_country,train.hotel_market
+ORDER BY num DESC LIMIT 3;
